0d5dfa73575ac8c1b6fb826bbeaee78a319cf34c
Yasith Jayawardana <yasith.jayawardana@icloud.com>
2018-04-22 19:39:22 +0530

    finished timer based lock

--- app/src/main/java/com/grydtech/goldline/HomeActivity.java
+++ app/src/main/java/com/grydtech/goldline/HomeActivity.java
@@ -1,15 +1,19 @@
 package com.grydtech.goldline;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.grydtech.goldline.fragments.AddItemFragment;
@@ -17,7 +21,10 @@ import com.grydtech.goldline.models.ItemType;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String password = "12344321";
     public static boolean isAuthorized = false;
+    public CountDownTimer countDownTimer;
+    public MenuItem statusText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,24 +63,59 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        this.getMenuInflater().inflate(R.menu.menu_home, menu);
+        statusText = menu.findItem(R.id.txt_lock_status);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will automatically handle clicks on
-        // the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml.
+        // the Home/Up button, so long as you specify a parent activity in AndroidManifest.xml
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_switch_lock) {
             if (isAuthorized) {
                 isAuthorized = false;
+                countDownTimer.cancel();
+                statusText.setTitle(R.string.str_locked);
                 item.setIcon(R.drawable.ic_lock_closed_24dp);
             } else {
-                isAuthorized = true;
-                item.setIcon(R.drawable.ic_lock_open_24dp);
+                countDownTimer = new CountDownTimer(30000, 1000) {
+                    public void onTick(long millisUntilFinished) {
+                        statusText.setTitle(String.format("Unlocked for %s s", millisUntilFinished / 1000));
+                    }
+
+                    public void onFinish() {
+                        isAuthorized = false;
+                        countDownTimer.cancel();
+                        statusText.setTitle(R.string.str_locked);
+                        item.setIcon(R.drawable.ic_lock_closed_24dp);
+                    }
+                };
+
+                final AlertDialog.Builder authDialogBuilder = new AlertDialog.Builder(this);
+                authDialogBuilder.setTitle("Authentication");
+                authDialogBuilder.setView(R.layout.fragment_auth);
+                authDialogBuilder.setPositiveButton(android.R.string.ok, null);
+                AlertDialog authDialog = authDialogBuilder.create();
+                authDialog.setOnShowListener(dialog -> {
+                    Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                    button.setOnClickListener(v -> {
+                        EditText passwordText = authDialog.findViewById(R.id.txt_password);
+                        if (passwordText != null && passwordText.getText().toString().equals(password)) {
+                            isAuthorized = true;
+                            item.setIcon(R.drawable.ic_lock_open_24dp);
+                            countDownTimer.start();
+                            // Create timer to show the countdown
+                            authDialog.dismiss();
+                        } else {
+                            Toast.makeText(this, "Invalid Password", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                });
+                authDialog.show();
             }
             return true;
         }
